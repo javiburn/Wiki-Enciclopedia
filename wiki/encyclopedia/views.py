f7579b84861044d4c8bd35b394f88ff7b2048a03
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-16 16:23:28 +0200

    converting .md files to .html format

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound, HttpResponse
 from . import util
+import markdown2
 
 # Create your views here.
 
@@ -11,4 +12,8 @@ def test(request):
     return HttpResponseNotFound()
 
 def search(request, name):
-    return HttpResponse(util.get_entry(name))
+    try:
+        html = markdown2.markdown(util.get_entry(name))
+    except:
+        return HttpResponseNotFound("<h1>Page not found</h1>")
+    return HttpResponse(html)
