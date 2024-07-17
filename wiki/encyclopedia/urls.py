d11f0362f21a4b3109fc91ffb2b504f657875ddc
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-17 15:11:57 +0200

    creating new entries

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ appname = "encyclopedia"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create", views.create, name="create"),
     path("wiki/<str:name>", views.search_by_url, name="search_url"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound, HttpResponse
+from django.urls import reverse
+from django import forms
+from django.http import HttpResponseNotFound, HttpResponse, HttpResponseRedirect
 from . import util
 import markdown2
 import glob
+import os
+from os.path import exists
+from django.core.exceptions import BadRequest
+
+class MyForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}))
 
 # Create your views here.
 def index(request):
@@ -41,3 +50,22 @@ def search_by_url(request, name):
             "name": name.title(), \
             "content": html
         })
+
+def create(request):
+    form = MyForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            path = "./entries/" + title + ".md"
+            if os.path.exists(path):
+                raise BadRequest('The entry already exists in the database.')
+            util.save_entry(title, content)
+            return render(request, "encyclopedia/entry.html", {
+            "name": title, \
+            "content": markdown2.markdown(util.get_entry(title))
+        })
+    else:
+        return render(request, "encyclopedia/create.html", {
+        "form": form
+    })
